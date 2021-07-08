bfd57620fbe3d96041ca5756dd809f8fb0eabbf9
Svetie <62491496+Svetie@users.noreply.github.com>
2021-07-07 21:34:53 -0400

    Add files via upload

---
+++ python_swap.py
@@ -0,0 +1,10 @@
+# // javascript code below!  
+# var arr = [1,3,5,7];
+# var temp = arr[0];
+# arr[0] = arr[1];
+# arr[1] = temp;
+
+# Python Swap
+arr = [1,3,5,7]
+arr[0], arr[1] = arr[1], arr[0]
+
